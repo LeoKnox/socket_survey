20ea326b8a825c0e5c07aab2337be080e7d26a5f
LeoKnox <LeoKnox@users.noreply.github.com>
2019-01-11 17:50:10 -0800

    Add files via upload
    using socket to emit data from client to server and back

--- server_orig.js
+++ server_orig.js
@@ -1,29 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + "/public"));
 const server = app.listen(1337);
 const io = require('socket.io')(server);
 var counter = 0;
 
-app.set('vews', __dirname + './static');
+app.set('vews', __dirname + './public');
 app.set('view engine', 'ejs');
 
 io.on('connection', function(socket) {
     socket.emit('greeting', {msg: 'Greetings, from server Node -server'});
     socket.on('thankyou', function(data) {
-        console.log(data.msg);})
-
-    socket.on('new-form', function(data) {
-        console.log(data);
-        socket.emit('form-data', data);
-    })
+        //$("div").text($("form").serialize());
+        console.log(data.msg);
+    });
 });
-
-app.get('/', function(req, res){ 
-    res.render('index');
-});
-
-app.get('/data', function(req, res){
-
-})
